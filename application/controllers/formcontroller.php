e222fdbd5f5df6c244a50181aad2396f3a43ad4a
Uttam Mohanty <mr.uttammohanty@gmail.com>
2015-05-14 01:59:33 +0530

    Form Submitt API handled

--- application/controllers/formcontroller.php
+++ application/controllers/formcontroller.php
@@ -24,9 +24,41 @@ class FormController extends Controller {
     public function afterAction(){
         $this->set_pageTitle('Moderate Posts');
     }
-
+ 
     public function api_form(){
+        $this->doNotRenderHeader = true;
+        $status = $message ='';
+        $get = $_POST;
+        if (!$get){
+            $status = 'error';
+            $message = 'Only POST Method allowed for Publish.';
+            
+        }else{
+            
+            if(!isset($get['title']) || empty($get['title']) ){
+                $status = 'error';
+                $message = 'Mandatory Field Title is Missing.';
+            }else{
+                $form = new Form();
+                $form->setTitle($get['title']);
+                $params = array();
+                foreach ($get as $key =>$data){
+                    if($key != 'title'){
+                        $params[$key]=$data;
+                    }
+                }
+                $form->setFormJson(json_encode($params));
+                #execute login process
+                $model = new FormRepository('Form');
+                $model->save($form);
+                $status = 'success';
+                $message = 'Form Data Submitted.';
+            }
         
+        }
+        echo json_encode(array('status' => $status, 'message' => $message));
+        exit;
+
     }
 
 } 
